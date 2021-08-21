e9ef22be5bdae5f789d2b5e59a1c8ee4c92e7fc3
Miguel Ferreira <angusmiguel21@gmail.com>
2021-08-21 17:51:00 +0100

    Refactored login

--- pages/api/users/[id].ts
+++ pages/api/users/[id].ts
@@ -1,6 +1,5 @@
 import { users } from '@mocks/users'
 
 export default async function getUsers({ query: { id } }: any, res: any) {
-    console.log('LOG ~ file: [id].ts ~ line 4 ~ id', id);
-    res.status(200).json(users.find(user => user.name === id))
+    return res.status(200).json(users.find(user => user.name === id))
 }
--- pages/restaurant/login/index.tsx
+++ pages/restaurant/login/index.tsx
@@ -1,20 +1,18 @@
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import { FaCheckCircle } from "react-icons/fa";
 import styles from "./styles.module.scss";
-import useSWR from "swr";
+import axios from "axios";
 import { useAuthentication } from "@shared/hooks/useAuthentication";
 
 export default function Login() {
   const [name, setName] = useState("");
-  const { data } = useSWR(`/api/users/${name || 'null'}`)
   const { setAuthenticated } = useAuthentication();
 
-  useEffect(() => {
-  }, [data])
-  
-  function handleSubmit(event: React.ChangeEvent<HTMLFormElement>) {
+
+  async function handleSubmit(event: React.ChangeEvent<HTMLFormElement>) {
     event.preventDefault();
-    setAuthenticated(!!data)
+    const { data: user } = await axios(`/api/users/${name}`);
+    setAuthenticated(!!user)
   }
 
   return (
