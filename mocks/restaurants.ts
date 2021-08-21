fa089e5d3f6259eb237ab019197605271d55a5bc
Miguel Ferreira <angusmiguel21@gmail.com>
2021-08-21 18:20:53 +0100

    Mockserver and local mocks

---
+++ mocks/restaurants.ts
@@ -0,0 +1,21 @@
+/* eslint-disable import/no-anonymous-default-export */
+export default {
+    "httpRequest": {
+        "method": "GET",
+        "path": "/restaurants"
+    },
+    "httpResponse": {
+        "statusCode": 200,
+        "body": [
+            {
+                "name": "tasco do zé"
+            },
+            {
+                "name": "churrasco típico 3"
+            }
+        ]
+    },
+    "times": {
+        "unlimited": true
+    }
+} 
