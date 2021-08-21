24d565ad37f72696af3c0452c7dd453cbc489fcc
Miguel Ferreira <angusmiguel21@gmail.com>
2021-08-21 01:50:22 +0100

    Mock data and mock endpoits

---
+++ mocks/users.ts
@@ -0,0 +1,21 @@
+/* eslint-disable import/no-anonymous-default-export */
+export default {
+    "httpRequest": {
+        "method": "GET",
+        "path": "/users"
+    },
+    "httpResponse": {
+        "statusCode": 200,
+        "body": [
+            {
+                "name": "tiago"
+            },
+            {
+                "name": "angus"
+            }
+        ]
+    },
+    "times": {
+        "unlimited": true
+    }
+}
